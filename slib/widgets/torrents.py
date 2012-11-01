27b1de0a8fc75ac6c26762b2691f6548541e79e4
Devaev Maxim <mdevaev@gmail.com>
2012-11-01 14:22:46 +0400

    Fixed torrents module path

--- slib/widgets/torrents.py
+++ slib/widgets/torrents.py
@@ -22,9 +22,9 @@ def torrentsList(torrents_dir_path, link_prefix) :
 	link_prefix = os.path.normpath(link_prefix)
 	size = 0
 	rows_list = []
-	for (torrent_file_name, meta_dict) in sorted(torrents.torrents(torrents_dir_path).items(), key=( lambda arg : arg[0] )) :
+	for (torrent_file_name, meta_dict) in sorted(tools.torrents.torrents(torrents_dir_path).items(), key=( lambda arg : arg[0] )) :
 		torrent_file_path = os.path.join(link_prefix, torrent_file_name)
-		torrent_size = torrents.torrentSize(meta_dict)
+		torrent_size = tools.torrents.torrentSize(meta_dict)
 		size += torrent_size
 		rows_list.append([
 				str(len(rows_list) + 1),
