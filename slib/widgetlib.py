cb42bea3349eed697a5477f5dade13ad56e0f353
Devaev Maxim <mdevaev@gmail.com>
2012-11-03 04:10:46 +0400

    widgetlib.buildWidgetsTree(): lookup only widgets

--- slib/widgetlib.py
+++ slib/widgetlib.py
@@ -10,7 +10,7 @@ import traceback
 ##### Public methods #####
 def provides(*macroses_list) :
 	def make_method(method) :
-		setattr(method, "is_widget", None)
+		setattr(method, "is_widget", True)
 		setattr(method, "widget_macroses_list", macroses_list)
 		def wrap(method, *args_list, **kwargs_dict) :
 			results_list = method(*args_list, **kwargs_dict)
@@ -29,7 +29,7 @@ def required(css_list=(), js_list=()) :
 	assert isinstance(css_list, (list, tuple)), "Required CSS must be a list"
 	assert isinstance(js_list, (list, tuple)), "Rquired JS must be a list"
 	def make_method(method) :
-		setattr(method, "is_widget", None)
+		setattr(method, "is_widget", True)
 		setattr(method, "widget_required_css", list(css_list))
 		setattr(method, "widget_required_js", list(js_list))
 		return method
@@ -45,17 +45,17 @@ def widgetRequired(method) :
 ###
 def buildWidgetsTree(module) :
 	widgets_tree_dict = {}
-	methods_dict = {}
+	widgets_dict = {}
 
 	for item_name in dir(module) :
 		item = getattr(module, item_name)
 		if isinstance(item, types.ModuleType) and (hasattr(item, "__file__") and "slib/widgets/" in item.__file__) :
-			(child_widgets_tree_dict, child_methods_dict) = buildWidgetsTree(item)
+			(child_widgets_tree_dict, child_widgets_dict) = buildWidgetsTree(item)
 			widgets_tree_dict[item_name] = child_widgets_tree_dict
-			methods_dict.update(child_methods_dict)
-		elif isinstance(item, types.FunctionType) :
+			widgets_dict.update(child_widgets_dict)
+		elif isinstance(item, types.FunctionType) and getattr(item, "is_widget", False) :
 			widgets_tree_dict[item_name] = item
-			methods_dict["%s.%s" % (item.__module__, item.__name__)] = item
+			widgets_dict["%s.%s" % (item.__module__, item.__name__)] = item
 
-	return (widgets_tree_dict, methods_dict)
+	return (widgets_tree_dict, widgets_dict)
 
