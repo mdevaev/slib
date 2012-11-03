b1c16eee2bf6e1252bd3c02ee6251b66974a78aa
Devaev Maxim <mdevaev@gmail.com>
2012-11-03 17:19:28 +0400

    Verbose user statistics

--- slib/widgets/kf.py
+++ slib/widgets/kf.py
@@ -13,11 +13,38 @@ from slib import validators
 import slib.validators.fs
 
 
+##### Public constants #####
+BERSERK_LEVELS_MAP = {
+	"MeleeDamageStat" : (0, 25000, 100000, 500000, 1500000, 3500000, 5500000),
+}
+SHARPSHOOTER_LEVELS_MAP = {
+	"HeadshotKillsStat" : (0, 30, 100, 700, 2500, 5500, 8500),
+}
+FIREBUG_LEVELS_MAP = {
+	"FlameThrowerDamageStat" : (0, 25000, 100000, 500000, 1500000, 3500000, 5500000),
+}
+FIELD_MEDIC_LEVELS_MAP = {
+	"DamageHealedStat" : (0, 200, 750, 4000, 12000, 25000, 100000),
+}
+DEMOLITIONS_LEVELS_MAP = {
+	"ExplosivesDamageStat" : (0, 25000, 100000, 500000, 1500000, 3500000, 5500000),
+}
+SUPPORT_SPEC_LEVELS_MAP = {
+	"WeldingPointsStat" : (0, 2000, 7000, 35000, 120000, 250000, 370000),
+	"ShotgunDamageStat" : (0, 25000, 100000, 500000, 1500000, 3500000, 5500000),
+}
+COMMANDO_LEVELS_MAP = {
+	"StalkerKillsStat" : (0, 30, 100, 350, 1200, 2400, 3600),
+	"BullpupDamageStat" : (0, 25000, 100000, 500000, 1500000, 3500000, 5500000),
+}
+
+
 ##### Public methods #####
 @widgetlib.provides("kf_leaderboard_table", "kf_perks_table")
-@widgetlib.required(css_list=("simple_table.css", "progress_bar.css", "script.css"), js_list=("spoiler.js",))
-def kfStatistics(config_file_path) :
+@widgetlib.required(css_list=("simple_table.css", "progress_bar.css"))
+def kfStatistics(config_file_path, profile_url) :
 	config_file_path = validators.fs.validAccessiblePath(config_file_path)
+	# FIXME: No validation for profile url!
 
 	stat_dict = serverPerksStat(config_file_path)
 
@@ -38,34 +65,18 @@ def kfStatistics(config_file_path) :
 	count = 1
 	players_list = []
 	for (user_id, player_stat_dict) in sorted(stat_dict.items(), key=( lambda arg : arg[1]["PlayerName"].lower() )) :
-		perk_percent = ( lambda option, limit : min(100 * player_stat_dict[option] / limit, 100) )
-		perk_progress = ( lambda percent : { "width" : "13%", "body" : html.progressBar(percent) } )
-
-		stat_table = html.statusTable([
-				("Kills", str(player_stat_dict["KillsStat"])),
-				("Time", tools.fmt.formatTimeDelta(player_stat_dict["TotalPlayTime"]))
-			])
-		(stat_spoiler_div_id, spoiler_title) = html.spoilerTitle("&raquo;")
-		stat_spoiler = html.spoilerBody(stat_spoiler_div_id, "<br>"+stat_table)
-
-		berserk = perk_percent("MeleeDamageStat", 5500000)
-		sharpshooter = perk_percent("HeadshotKillsStat", 8500)
-		firebug = perk_percent("FlameThrowerDamageStat", 5500000)
-		field_medic = perk_percent("DamageHealedStat", 100000)
-		demolitions = perk_percent("ExplosivesDamageStat", 5500000)
-		support_spec = (perk_percent("WeldingPointsStat", 370000) + perk_percent("ShotgunDamageStat", 5500000)) / 2
-		commando = (perk_percent("StalkerKillsStat", 3600) + perk_percent("BullpupDamageStat", 5500000)) / 2
-
+		perk_progress = ( lambda levels_map : { "width" : "13%", "body" : html.progressBar(calculateProgress(levels_map, player_stat_dict)) } )
+		player_name = "<a href=\"%s\">&raquo;</a>&nbsp;%s" % (profile_url % { "user_id" : user_id }, player_stat_dict["PlayerName"])
 		players_list.append([
 				str(count),
-				{ "nowrap" : None, "body" : "%s %s%s" % (spoiler_title, player_stat_dict["PlayerName"], stat_spoiler) },
-				perk_progress(berserk),
-				perk_progress(sharpshooter),
-				perk_progress(firebug),
-				perk_progress(field_medic),
-				perk_progress(demolitions),
-				perk_progress(support_spec),
-				perk_progress(commando)
+				{ "nowrap" : None, "body" : player_name },
+				perk_progress(BERSERK_LEVELS_MAP),
+				perk_progress(SHARPSHOOTER_LEVELS_MAP),
+				perk_progress(FIREBUG_LEVELS_MAP),
+				perk_progress(FIELD_MEDIC_LEVELS_MAP),
+				perk_progress(DEMOLITIONS_LEVELS_MAP),
+				perk_progress(SUPPORT_SPEC_LEVELS_MAP),
+				perk_progress(COMMANDO_LEVELS_MAP),
 			])
 		count += 1
 
@@ -73,6 +84,46 @@ def kfStatistics(config_file_path) :
 	players_table = html.tableWithHeader(players_header_list, players_list)
 
 	return (leaderboard_table, players_table)
+
+
+@widgetlib.provides("kf_player_stat_table")
+@widgetlib.required(css_list=("simple_table.css", "progress_bar.css"))
+def kfPlayerStatistics(user_id, config_file_path) :
+	user_id = validators.common.validNumber(user_id, 1)
+	config_file_path = validators.fs.validAccessiblePath(config_file_path)
+
+	stat_dict = serverPerksStat(config_file_path)
+	if not user_id in stat_dict :
+		raise RuntimeError("Invalid Steam ID")
+	stat_dict = stat_dict[user_id]
+
+	wins = stat_dict["WinsCount"]
+	losts = stat_dict["LostsCount"]
+	battles = wins + losts
+	skill_factor = float(wins) / float(battles)
+	skill_factor_text = ( """
+			<div style="float:left; text-align:left;">%.2f&nbsp;</div>
+			<div style="float:right; text-align:right; width:100px">%s</div>
+		""" % (skill_factor, html.progressBar(skill_factor * 100)) )
+
+	player_stat_table = html.statusTable([
+			("Battles", str(battles)),
+			("Wins", str(wins)),
+			("Losts", str(losts)),
+			("Skill factor", skill_factor_text),
+			("Last hope", str(stat_dict["SoleSurvivorWavesStat"])),
+			("Kills", str(stat_dict["KillsStat"])),
+			("Time played", tools.fmt.formatTimeDelta(stat_dict["TotalPlayTime"])),
+			#map(str, calculateLevelProgress(BERSERK_LEVELS_MAP, stat_dict)),
+			#map(str, calculateLevelProgress(SHARPSHOOTER_LEVELS_MAP, stat_dict)),
+			#map(str, calculateLevelProgress(FIREBUG_LEVELS_MAP, stat_dict)),
+			#map(str, calculateLevelProgress(FIELD_MEDIC_LEVELS_MAP, stat_dict)),
+			#map(str, calculateLevelProgress(DEMOLITIONS_LEVELS_MAP, stat_dict)),
+			#map(str, calculateLevelProgress(SUPPORT_SPEC_LEVELS_MAP, stat_dict)),
+			#map(str, calculateLevelProgress(COMMANDO_LEVELS_MAP, stat_dict)),
+		])
+
+	return (player_stat_table,)
 
 
 ##### Private methods #####
@@ -93,4 +144,22 @@ def serverPerksStat(config_file_path) :
 			stat_dict[user_id][option] = value
 
 	return stat_dict
+
+def calculateProgress(levels_map, stat_dict) :
+	percent = 0
+	for (key, levels_list) in levels_map.iteritems() :
+		percent += min(100 * stat_dict[key] / levels_list[-1], 100)
+	return percent / len(levels_map)
+
+def calculateLevelProgress(levels_map, stat_dict) :
+	percent = 0
+	result_level = max_level = len(levels_map.values()[0]) -1
+	for (key, levels_list) in levels_map.iteritems() :
+		for level in range(len(levels_list) - 1, -1, -1) :
+			if stat_dict[key] >= levels_list[level] :
+				result_level = min(level, result_level)
+				for_next_level = levels_list[( level + 1 if level < max_level else max_level )]
+				percent += min(100 * stat_dict[key] / for_next_level, 100)
+				break
+	return (result_level, percent / len(levels_map))
 
