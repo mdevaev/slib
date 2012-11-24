61a778c3da84f00b7817aaf98fb8212a15298449
Devaev Maxim <mdevaev@gmail.com>
2012-11-25 01:57:44 +0400

    widgets: Optional kf, steam and torrents

--- slib/widgets/__init__.py
+++ slib/widgets/__init__.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 
+
 from . import system
-from . import torrents
 from . import mlocate
 from . import img
-from . import steam
-from . import kf
 from . import yandex
 from . import google
 from . import twitter
+
+try :
+	from . import torrents
+except ImportError :
+	torrents = None
+
+try :
+	from . import steam
+except ImportError :
+	steam = None
+
+try :
+	from . import kf
+except ImportError :
+	kf = None
 
