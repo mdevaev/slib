1f6bd593164d56aa7f8ba29108053c92885c3738
Devaev Maxim <mdevaev@gmail.com>
2013-05-25 04:03:54 +0400

    torrents: using rtlib instead of local crutches

--- slib/widgets/torrents.py
+++ slib/widgets/torrents.py
@@ -2,8 +2,9 @@
 
 
 import os
-import bencode
 import operator
+
+from rtlib import tfile
 
 from slib import widgetlib
 from slib import html
@@ -21,37 +22,19 @@ import slib.validators.fs
 def torrentsList(torrents_dir_path, link_prefix) :
 	torrents_dir_path = validators.fs.validAccessiblePath(torrents_dir_path)
 	link_prefix = os.path.normpath(link_prefix)
+
 	size = 0
 	rows_list = []
-	for (torrent_file_name, meta_dict) in sorted(torrents(torrents_dir_path).items(), key=operator.itemgetter(0)) :
-		torrent_size = torrentSize(meta_dict)
+	for (torrent_file_name, torrent) in sorted(tfile.torrents(torrents_dir_path).items(), key=operator.itemgetter(0)) :
+		torrent_size = torrent.size()
 		size += torrent_size
 		rows_list.append([
 				str(len(rows_list) + 1),
 				"<a href=\"%s\">%s</a>" % (os.path.join(link_prefix, torrent_file_name), torrent_file_name),
 				tools.fmt.formatSize(torrent_size),
-				html.maybeLink(meta_dict.get("comment", ""))
+				html.maybeLink(torrent.comment()),
 			])
-	torrents_table = html.tableWithHeader(["N", "Torrent", "Size", "Comment"], rows_list)
+	torrents_table = html.tableWithHeader(["N", "Name", "Size", "Comment"], rows_list)
+
 	return (torrents_table, str(len(rows_list)), tools.fmt.formatSize(size))
-
-
-##### Private methods #####
-def torrents(torrents_dir_path) :
-	torrents_dict = {}
-	for torrent_file_name in filter(lambda name : name.endswith(".torrent"), os.listdir(torrents_dir_path)) :
-		with open(os.path.join(torrents_dir_path, torrent_file_name)) as torrent_file :
-			torrents_dict[torrent_file_name] = bencode.bdecode(torrent_file.read())
-	return torrents_dict
-
-def torrentSize(meta_dict) :
-	if meta_dict["info"].has_key("length") :
-		return meta_dict["info"]["length"]
-	elif meta_dict["info"].has_key("files") :
-		size = 0
-		for part_dict in meta_dict["info"]["files"] :
-			size += part_dict["length"]
-		return size
-	else :
-		return -1
 
